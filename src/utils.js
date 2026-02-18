7f03540efe09df78d37c1b9497c6d21041d17547
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-02-18 08:37:48 +0000

    add general utils file

---
+++ src/utils.js
@@ -0,0 +1,17 @@
+/**
+ *  SOURCE_CODE_VERSION: 1.0.0
+ *  PROGRAMMER/DEVELOPER: NELSON CHIDI (NELMATRIX)
+ *  DATE_TIME: FEB 18, 2026; 08:20AM
+ *  REPOSITORY: https://github.com/NelMatrix743/movie_watchlist_api.git
+ */
+
+
+// shortern console.log
+function print(msg){
+    console.log(msg);
+}
+
+
+module.exports = {
+    print
+};
